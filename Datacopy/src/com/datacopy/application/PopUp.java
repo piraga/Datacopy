15abd285e2d0f29122ac9c62951c2cbbfc8b8d55
10298 <10298@DINCD-063.DSIINDIA.COM>
2019-05-03 18:36:16 +0530

    Add popup stage

--- Datacopy/src/com/datacopy/application/PopUp.java
+++ Datacopy/src/com/datacopy/application/PopUp.java
@@ -9,15 +9,21 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.ChoiceDialog;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
 
 public class PopUp {
 	
 	public Alert alert=null;
+	public Stage stage;
 	public boolean processPermission(String uname, String pass, String port, String host, String sid,String sname) {
 		
 		alert = new Alert(AlertType.CONFIRMATION);
 		alert.setTitle("Confirmation Dialog");
 		alert.setHeaderText("Make sure database credentials is correct ?");
+		stage = (Stage) alert.getDialogPane().getScene().getWindow();
+		stage.getIcons().add(new Image("logo.png")); // To add an icon
+		
 		alert.setContentText("Username="+uname+"\n"+"Password="+pass+"\n"+
 		"Port="+port+"\n"+"Hostname="+host+"\n"+"SID="+sid+"\n"+"ServiceName="+sname);
 		
@@ -34,6 +40,8 @@ public class PopUp {
 		alert= new Alert (AlertType.ERROR);
 		alert.setTitle("Connection Failed");
 		alert.setHeaderText("Please check database credentials");
+		stage = (Stage) alert.getDialogPane().getScene().getWindow();
+		stage.getIcons().add(new Image("logo.png")); // To add an icon
 		alert.showAndWait();
 		
 	}
@@ -42,6 +50,8 @@ public class PopUp {
 		alert= new Alert (AlertType.INFORMATION);
 		alert.setTitle("ExportFile Loaction");
 		alert.setHeaderText("File Location: ");
+		stage = (Stage) alert.getDialogPane().getScene().getWindow();
+		stage.getIcons().add(new Image("logo.png")); // To add an icon
 		alert.setContentText(location);
 		alert.showAndWait();
 	}
@@ -74,12 +84,16 @@ public class PopUp {
 			alert=new Alert(AlertType.WARNING);
 			alert.setTitle("Warning PopUp");
 			alert.setHeaderText("Account ID is Emplty ");
+			stage = (Stage) alert.getDialogPane().getScene().getWindow();
+			stage.getIcons().add(new Image("logo.png")); // To add an icon
 			alert.showAndWait();
 			throw new Exception();			
 		}else if(isAcctSec && secId.trim().isEmpty()){
 			alert=new Alert(AlertType.WARNING);
 			alert.setTitle("Warning PopUp");
 			alert.setHeaderText("Security ID is Emplty ");
+			stage = (Stage) alert.getDialogPane().getScene().getWindow();
+			stage.getIcons().add(new Image("logo.png")); // To add an icon
 			alert.showAndWait();
 			throw new Exception();	
 			
